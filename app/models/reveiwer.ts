5a34a17634f4005812945bd79cf1ec3ffe5451c0
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-30 09:10:29 +0700

    feat: add reviewer model and migration

---
+++ app/models/reveiwer.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Reveiwer extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare reveiwerId: number
+
+  @column()
+  declare umkmDataId: number
+
+  @column()
+  declare rating: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
