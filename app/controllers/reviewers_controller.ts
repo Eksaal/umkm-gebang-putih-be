9d5ad359cd449f0eec94f243163ef2acac3202c3
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-05-30 09:19:27 +0700

    feat: add reveiwer controller

---
+++ app/controllers/reviewers_controller.ts
@@ -0,0 +1,83 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import Reveiwer from '#models/reveiwer'
+import { responseUtil } from '../../helper/response_util.js'
+import vine, { SimpleMessagesProvider } from '@vinejs/vine'
+
+export default class ReviewersController {
+  public async index({ response }: HttpContext) {
+    const reviewers = await Reveiwer.all()
+    return responseUtil.success(response, reviewers, 'Reviewers retrieved successfully')
+  }
+
+  public async show({ params, response }: HttpContext) {
+    const reviewer = await Reveiwer.find(params.id)
+    if (!reviewer) {
+      return responseUtil.notFound(response, 'Reviewer not found')
+    }
+    return responseUtil.success(response, reviewer, 'Reviewer retrieved successfully')
+  }
+
+  public async store({ request, response }: HttpContext) {
+    const data = await vine
+      .compile(
+        vine.object({
+          reviewerId: vine.number(),
+          umkmDataId: vine.number(),
+          rating: vine.number(),
+        })
+      )
+      .validate(request.all(), {
+        messagesProvider: new SimpleMessagesProvider({
+          'required': 'The {{ field }} field is required.',
+        }),
+      })
+
+    const reviewer = await Reveiwer.create(data)
+
+    return responseUtil.created(response, reviewer, 'Reviewer created successfully')
+  }
+
+  public async update({ params, request, response }: HttpContext) {
+    const reviewer = await Reveiwer.find(params.id)
+    if (!reviewer) {
+      return responseUtil.notFound(response, 'Reviewer not found')
+    }
+
+    const data = await vine
+      .compile(
+        vine.object({
+          reviewerId: vine.number().optional(),
+          umkmDataId: vine.number().optional(),
+          rating: vine.number().optional(),
+        })
+      )
+      .validate(request.all(), {
+        messagesProvider: new SimpleMessagesProvider({
+          'required': 'The {{ field }} field is required.',
+        }),
+      })
+
+    reviewer.merge(data)
+    await reviewer.save()
+
+    return responseUtil.success(response, reviewer, 'Reviewer updated successfully')
+  }
+
+  public async destroy({ params, response }: HttpContext) {
+    const reviewer = await Reveiwer.find(params.id)
+    if (!reviewer) {
+      return responseUtil.notFound(response, 'Reviewer not found')
+    }
+
+    await reviewer.delete()
+    return responseUtil.noContent(response, 'Reviewer deleted successfully')
+  }
+
+  public async getReviewByUmkmDataId({ params, response }: HttpContext) {
+    const reviews = await Reveiwer.query().where('umkmDataId', params.umkmDataId)
+    if (reviews.length === 0) {
+      return responseUtil.notFound(response, 'No reviews found for the specified UMKM Data ID')
+    }
+    return responseUtil.success(response, reviews, 'Reviews retrieved successfully')
+  }
+}
