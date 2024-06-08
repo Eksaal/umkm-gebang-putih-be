81dc7ae9d0e8770954cd3d5df4cbe93a351f76c5
Agus Darmawan <5024211070@mhs.its.ac.id>
2024-06-09 02:29:28 +0700

    fix: routes in picture

--- start/routes/v1/picture.ts
+++ start/routes/v1/picture.ts
@@ -5,7 +5,6 @@ export default function pictureRoutes(){
     router.group(() => {
         router.get('/', [UmkmPicturesController, 'index'])
         router.post('/', [UmkmPicturesController,'store'])
-        router.get('/:id', [UmkmPicturesController,'show'])
         router.get('/umkm/:id', [UmkmPicturesController,'showByUmkmDataId'])
         router.patch('/:id', [UmkmPicturesController, 'update'])
         router.delete('/:id', [UmkmPicturesController, 'destroy'])
